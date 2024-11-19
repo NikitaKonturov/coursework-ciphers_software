87bdd14ad7b8e966e30679eb383eb58f1a67fa66
NikitaKonturov <nikitaconturov@gmail.com>
2024-11-19 16:13:37 +0300

    Исправил htm

--- FastApi/static/changeCipherScript.js
+++ FastApi/static/changeCipherScript.js
@@ -43,29 +43,78 @@ async function addBlockOfKeysSettings() {
             keysSettingBlock.appendChild(divParametr)
         });
 
+        
+        if(document.getElementById("keys-choose-block") != null) {
+            document.getElementById("keys-choose-block").remove()
+        }
         document.getElementById("main-keys-block").appendChild(keysSettingBlock)
         
     } catch (error) {
-        console.error("Response error:", error);
+        console.error("Response error: ", error);
+    }
+}
+
+async function addBlockOfGetUsersKeys() {
+    try {
+        let blockWithChooseKey = document.createElement("div")
+        blockWithChooseKey.id = "keys-choose-block"
+        
+        let labelChooseElement = document.createElement("label")
+        labelChooseElement.id = "choose-keys-label"
+        labelChooseElement.textContent = "Choose file with keys"
+        labelChooseElement.htmlFor = "input-keys"
+
+        let inputUsersKeys = document.createElement("input")
+        inputUsersKeys.id = "keys-file"
+        inputUsersKeys.name = "keys_file"
+        inputUsersKeys.type = "file"
+        inputUsersKeys.addEventListener('change', function(){document.getElementById('choose-keys-label').textContent = this.files[0].name}, true)
+        inputUsersKeys.accept = ".doc,.docx,application/msword, text/plain"
+        
+        blockWithChooseKey.appendChild(labelChooseElement)
+        blockWithChooseKey.appendChild(inputUsersKeys)
+        
+        if(document.getElementById("keys-settings-block") != null) {
+            document.getElementById("keys-settings-block").remove()
+        }
+        document.getElementById("main-keys-block").appendChild(blockWithChooseKey)
+    } catch(error) {
+        console.error(error)
     }
 }
 
 
 
-async function changeCipher() {
+async function encriptSettings() {
     try {
-        let selectKyesTypeBlock = document.createElement("div")
-        selectKyesTypeBlock.id = "select-type-keys-block"
-        let autoGenKeysButton = document.createElement("button")
-        autoGenKeysButton.textContent = "Auto keys generation"
-        autoGenKeysButton.addEventListener('click', addBlockOfKeysSettings, true)
-        selectKyesTypeBlock.appendChild(autoGenKeysButton)
-        
-        let rigthBlock = document.createElement("div")
-        rigthBlock.id = "main-keys-block"
-        rigthBlock.appendChild(selectKyesTypeBlock)
+        if(document.getElementById("ciphersList").value == "Empty_tag") {
+            alert("Сhoose a cipher!")
+        } else {
+            if(document.getElementById("count-of-tg").value <= 0 || document.getElementById("lenght-of-tg").value <= 0) {
+                alert("Count of telegrmas and size of telegrams must be nutural")
+            }
+            let selectKyesTypeBlock = document.createElement("div")
+            selectKyesTypeBlock.id = "select-type-keys-block"
+            let autoGenKeysButton = document.createElement("button")
+            autoGenKeysButton.textContent = "Auto keys generation"
+            autoGenKeysButton.addEventListener('click', addBlockOfKeysSettings, true)
+            selectKyesTypeBlock.appendChild(autoGenKeysButton)
 
-        document.body.appendChild(rigthBlock)
+            let getUserKeysButton = document.createElement("button")
+            getUserKeysButton.textContent = "Select keys"
+            getUserKeysButton.addEventListener('click', addBlockOfGetUsersKeys, true)
+            selectKyesTypeBlock.appendChild(getUserKeysButton)
+
+
+
+            let rigthBlock = document.createElement("div")
+            rigthBlock.id = "main-keys-block"
+            rigthBlock.appendChild(selectKyesTypeBlock)
+                
+
+            document.body.appendChild(rigthBlock)
+
+        }
     }
     catch(err){
         console.error("Error occurred:", err);
