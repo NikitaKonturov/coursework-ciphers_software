ed3cfbd7706b478346d332e0205c005237764108
NikitaKonturov <nikitaconturov@gmail.com>
2025-02-11 21:35:20 +0300

    Добавил SHA-256

---
+++ HMAC_DRBG/HMAC/HMAC.cpp
@@ -0,0 +1,8 @@
+#include "HMAC.hpp"
+
+
+std::bitset<256> get_entropy() {
+
+
+
+}
---
+++ HMAC_DRBG/HMAC/HMAC.hpp
@@ -0,0 +1,13 @@
+#ifndef HMAC_HPP
+#define HMAC_HPP
+#include <iostream>
+#include <bitset>
+
+
+// Собираем энтропию с помощю аппаратных методов 
+std::bitset<256> get_entropy();
+
+
+
+
+#endif // HMAC_HPP
