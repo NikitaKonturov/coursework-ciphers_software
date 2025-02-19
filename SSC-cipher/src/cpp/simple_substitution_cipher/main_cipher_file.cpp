663cf2bf32e2634cbc81b19a7a07cd5ee162b99a
NikitaKonturov <nikitaconturov@gmail.com>
2025-02-19 20:25:27 +0300

    Не в этом проблема

--- SSC-cipher/src/cpp/simple_substitution_cipher/main_cipher_file.cpp
+++ SSC-cipher/src/cpp/simple_substitution_cipher/main_cipher_file.cpp
@@ -6,7 +6,6 @@
 #include <pybind11/stl.h>
 
 
-
 // главный макрос позволяющий экспортировать функции и типы данных в python
 PYBIND11_MODULE(Simple_substitution_cipher, m) {
     m.doc() = "Simple_substitution_cipher";
@@ -14,14 +13,7 @@ PYBIND11_MODULE(Simple_substitution_cipher, m) {
     m.def("gen_keys", &gen_keys); // аналогично
     m.def("get_key_propertys", &get_key_propertys); // аналогично
     m.def("decript", &decript); // аналогично
-    static pybind11::exception<std::exception> base_exception(m, "cppException");
-    pybind11::register_exception_translator([](std::exception_ptr p) {
-        try {
-            if (p) std::rethrow_exception(p);
-        } catch (const std::exception& e) {
-            PyErr_SetString(PyExc_RuntimeError, e.what());
-        }
-    });
+
     pybind11::register_exception<KeyPropertyError>(m, "KeyPropertyError"); // регистрация класса ошибки валидности свойств ключа
     pybind11::register_exception<InvalidKey>(m, "InvalidKey"); // регистрация класса ошибки валидности ключа
     pybind11::register_exception<InvalidOpenText>(m, "InvalidOpenText"); // регистрация класса валидности открытого текста
