befaf771855d416dc97e8dec1277faeaa247c4e0
KonstantinVrunouski <enotsith@gmail.com>
2025-02-19 19:40:21 +0300

    Before disaster

--- FastApi/exception_handlers.py
+++ FastApi/exception_handlers.py
@@ -30,5 +30,5 @@ async def key_property_error_exception(request, exc: KeyPropertyError):
     return JSONResponse(status_code=405, content={"error": "Key property error", "detail": str(exc)})
 
 
-async def unknown_exception(request, exc: Exception):
+async def unknown_exception(request, exc: RuntimeError):
     return JSONResponse(status_code=500, content={"error": "An unexpected error occurred", "detail": str(exc)})
