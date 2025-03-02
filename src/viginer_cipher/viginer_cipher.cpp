f4f25b3eaa3d335f7f2b97dc284fd058ecfdb15c
VladislavYurkov <Dostupnii579@gmail.com>
2025-03-02 16:54:20 +0300

    Начал всё заново

--- src/viginer_cipher/viginer_cipher.cpp
+++
@@ -1,3 +0,0 @@
-#include "viginer_cipher.hpp"
-
-
--- src/viginer_key_gen/viginer_key_gen.hpp
+++ src/viginer_key_gen/viginer_key_gen.hpp
@@ -9,19 +9,4 @@
 #include <random>
 #include <set>
 
-bool is_rus_alpha(uint8_t symbol);
-
-std::map<size_t, size_t> count_words(const std::string& keyFilePath, const std::string& lang);
-
-void set_final_key_word_num(size_t& keyWordNum, bool*& bunnedWords);
-
-std::string give_word(
-    const std::string& keyFilePath, 
-    const std::string& lang, 
-    std::map<size_t, size_t>& wordsCount,
-    const size_t& keyWordLength,
-    bool*& bannedWords,
-    const size_t& allWordsCount
-);
-
 #endif //VIGINER_KEY_GEN_HPP
