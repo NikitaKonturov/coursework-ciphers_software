1a451fb5511c9489066249da4b822723e6e2e3af
NikitaKonturov <nikitaconturov@gmail.com>
2025-03-04 13:24:58 +0300

    Ошибка js

--- FastApi/static/changeCipherScript.js
+++ FastApi/static/changeCipherScript.js
@@ -304,3 +304,4 @@ export async function encriptSettings() {
 
 window.encriptSettings = encriptSettings
 window.preventActionButton = preventActionButton
+window.sendDecriptRequest = sendDecriptRequest
