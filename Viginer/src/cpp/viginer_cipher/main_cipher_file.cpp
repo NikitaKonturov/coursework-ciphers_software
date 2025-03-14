f9869a8cc719fb69fa333be95775c4f9a199187d
VladislavYurkov <dostupnii579@gmail.com>
2025-03-14 14:49:11 +0300

    Собираемая версия проекта

--- Viginer/src/cpp/viginer_cipher/main_cipher_file.cpp
+++ Viginer/src/cpp/viginer_cipher/main_cipher_file.cpp
@@ -8,7 +8,7 @@
 
 
 // главный макрос позволяющий экспортировать функции и типы данных в python
-PYBIND11_MODULE(viginer_cipher, m) {
+PYBIND11_MODULE(Viginer_cipher, m) {
     m.doc() = "Шифр Виженера";
     m.def("encript", &encript);
     m.def("decript", &decript);
