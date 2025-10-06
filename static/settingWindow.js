c22c8cd1c526b8babbca48898389a67eafb963f2
NikitaKonturov <nikitaconturov@gmail.com>
2025-10-06 20:22:13 +0300

    for me

--- static/settingWindow.js
+++ static/settingWindow.js
@@ -1,7 +1,7 @@
 interfaceLanguage = 'ru';
 cipherLanguage = 'ru';
-encryptFolderPath = 'C:\Disk\ G\Test\Test_cipher';
-decryptFolderPath = 'C:\Disk\ G\Test\Test_cipher';
+encryptFolderPath = 'C:\Development\course_work';
+decryptFolderPath = 'C:\Development\course_work';
 fiveGramsEnabled = 'false'
 
 function showError(message) {
