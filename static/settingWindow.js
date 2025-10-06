8871c06ca4c4cda18b9c6c838ffb3b98b6ce5279
RostislavStarikov <starikovrostislav7@gmail.com>
2025-10-06 19:30:26 +0300

    Removed iterator numbers at the end of keys in the homophone cipher

--- static/settingWindow.js
+++ static/settingWindow.js
@@ -1,7 +1,7 @@
 interfaceLanguage = 'ru';
 cipherLanguage = 'ru';
-encryptFolderPath = 'Cardano';
-decryptFolderPath = 'Cardano';
+encryptFolderPath = 'C:\Disk\ G\Test\Test_cipher';
+decryptFolderPath = 'C:\Disk\ G\Test\Test_cipher';
 fiveGramsEnabled = 'false'
 
 function showError(message) {
