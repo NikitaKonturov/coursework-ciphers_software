0c58204fdae5d3273ab9d6d63a951927fe037013
RostislavStarikov <starikovrostislav7@gmail.com>
2025-10-09 00:09:35 +0300

    Изменил омофонный шифр:добавлена проверка на корректность пользоватьельских ключей

--- static/settingWindow.js
+++ static/settingWindow.js
@@ -1,7 +1,7 @@
 interfaceLanguage = 'ru';
 cipherLanguage = 'ru';
-encryptFolderPath = 'C:\Development\course_work';
-decryptFolderPath = 'C:\Development\course_work';
+encryptFolderPath = 'C:\Disk\ G\Test\Test_cipher';
+decryptFolderPath = 'C:\Disk\ G\Test\Test_cipher';
 fiveGramsEnabled = 'false'
 
 function showError(message) {
